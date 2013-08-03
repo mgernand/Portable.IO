9ce76a3f99c6ffd1e823d72e8b29df82e390eac2
mgernand <m.gernand@mge-it.de>
2013-08-03 19:56:35 +0200

    Added portable library test project.

--- Source/Portable.IO.Tests/Properties/AssemblyInfo.cs
+++ Source/Portable.IO.Tests/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Portable.IO.Net.Tests")]
+[assembly: AssemblyTitle("Portable.IO.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Portable.IO.Net.Tests")]
+[assembly: AssemblyProduct("Portable.IO.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ce0336cf-c896-4583-b537-81cc219073ad")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
