14295838ea0d357143208953b649670aed3f7ace
mgernand <m.gernand@mge-it.de>
2013-08-03 20:27:28 +0200

    Added test project.

---
+++ Source/Portable.IO.Tests/DirectoryTests.cs
@@ -0,0 +1,225 @@
+﻿namespace Portable.IO.Tests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class DirectoryTests : TestBase
+	{
+		[Test]
+		public void ShouldCreateFile()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IFile file = directory.CreateFile("demo.txt");
+
+			//// Assert
+			//file.Should().NotBeNull();
+			//file.Exists.Should().BeTrue();
+			//file.Delete();
+		}
+
+		[Test]
+		public async void ShouldCreateFileAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IFile file = await directory.CreateFileAsync("demo.txt");
+
+			//// Assert
+			//file.Should().NotBeNull();
+			//file.Exists.Should().BeTrue();
+			//file.Delete();
+		}
+
+		[Test]
+		public void ShouldGetFile()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IFile file = directory.GetFile("file.txt");
+
+			//// Assert
+			//file.Should().NotBeNull();
+			//file.Exists.Should().BeTrue();
+		}
+
+		[Test]
+		public async void ShouldGetFileAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IFile file = await directory.GetFileAsync("file.txt");
+
+			//// Assert
+			//file.Should().NotBeNull();
+			//file.Exists.Should().BeTrue();
+		}
+
+		[Test]
+		public void ShouldGetFiles()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IEnumerable<IFile> files = directory.GetFiles();
+
+			//// Assert
+			//files.Count().Should().Be(1);
+		}
+
+		[Test]
+		public async void ShouldGetFilesAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IEnumerable<IFile> files = await directory.GetFilesAsync();
+
+			//// Assert
+			//files.Count().Should().Be(1);
+		}
+
+		[Test]
+		public void ShouldCreateDirectory()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//string name = Guid.NewGuid().ToString();
+			//IDirectory newDirectory = directory.CreateDirectory(name);
+
+			//// Assert
+			//newDirectory.Should().NotBeNull();
+			//newDirectory.Exists.Should().BeTrue();
+			//newDirectory.Delete();
+		}
+
+		[Test]
+		public async void ShouldCreateDirectoryAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//string name = Guid.NewGuid().ToString();
+			//IDirectory newDirectory = await directory.CreateDirectoryAsync(name);
+
+			//// Assert
+			//newDirectory.Should().NotBeNull();
+			//newDirectory.Exists.Should().BeTrue();
+			//newDirectory.Delete();
+		}
+
+		[Test]
+		public void ShouldGetDirectory()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IDirectory subDirectory = directory.GetDirectory("Subfolder");
+
+			//// Assert
+			//subDirectory.Should().NotBeNull();
+			//subDirectory.Exists.Should().BeTrue();
+		}
+
+		[Test]
+		public async void ShouldGetDirectoryAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IDirectory subDirectory = await directory.GetDirectoryAsync("Subfolder");
+
+			//// Assert
+			//subDirectory.Should().NotBeNull();
+			//subDirectory.Exists.Should().BeTrue();
+		}
+
+		[Test]
+		public void ShouldGetDirectories()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IEnumerable<IDirectory> directories = directory.GetDirectories();
+
+			//// Assert
+			//directories.Count().Should().Be(1);
+		}
+
+		[Test]
+		public async void ShouldGetDirectoriesAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Act
+			//IEnumerable<IDirectory> directories = await directory.GetDirectoriesAsync();
+
+			//// Assert
+			//directories.Count().Should().Be(1);
+		}
+
+		[Test]
+		public void ShouldDeleteDirectory()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+			//IDirectory temp = directory.CreateDirectory("Temp");
+			//temp.Exists.Should().BeTrue();
+
+			//// Act
+			//temp.Delete();
+
+			//// Assert
+			//temp.Exists.Should().BeFalse();
+		}
+
+		[Test]
+		public async void ShouldDeleteDirectoryAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+			//IDirectory temp = directory.CreateDirectory("Temp");
+			//temp.Exists.Should().BeTrue();
+
+			//// Act
+			//await temp.DeleteAsync();
+
+			//// Assert
+			//temp.Exists.Should().BeFalse();
+		}
+	}
+}
---
+++ Source/Portable.IO.Tests/FileSystemTests.cs
@@ -0,0 +1,78 @@
+﻿namespace Portable.IO.Tests
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class FileSystemTests : TestBase
+	{
+		//[Test]
+		//public void ShouldCreateFileSystem()
+		//{
+		//	// Arrange & Act
+		//	IFileSystem fileSystem = FileSystem.Current;
+
+		//	// Assert
+		//	(fileSystem is NetFileSystem).Should().BeTrue();
+		//}
+
+		[Test]
+		public void ShouldGetFile()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+
+			//// Act
+			//IFile file = fileSystem.GetFile("Files/file.txt");
+
+			//// Assert
+			//file.Should().NotBeNull();
+			//file.Exists.Should().BeTrue();
+			//file.Name.Should().Be("file.txt");
+		}
+
+		[Test]
+		public async void ShouldGetFileAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+
+			//// Act
+			//IFile file = await fileSystem.GetFileAsync("Files/file.txt");
+
+			//// Assert
+			//file.Should().NotBeNull();
+			//file.Exists.Should().BeTrue();
+			//file.Name.Should().Be("file.txt");
+		}
+
+		[Test]
+		public void ShouldGetDirectory()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+
+			//// Act
+			//IDirectory directory = fileSystem.GetDirectory("Files");
+
+			//// Assert
+			//directory.Should().NotBeNull();
+			//directory.Exists.Should().BeTrue();
+			//directory.Name.Should().Be("Files");
+		}
+
+		[Test]
+		public async void ShouldGetDirectoryAsync()
+		{
+			//// Arrange
+			//IFileSystem fileSystem = FileSystem.Current;
+
+			//// Act
+			//IDirectory directory = await fileSystem.GetDirectoryAsync("Files");
+
+			//// Assert
+			//directory.Should().NotBeNull();
+			//directory.Exists.Should().BeTrue();
+			//directory.Name.Should().Be("Files");
+		}
+	}
+}
